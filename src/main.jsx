17e76d16dd74b50e2e59480d6117654a87fd7439
Jean-Paul Niko <164699514+jeanpaulniko@users.noreply.github.com>
2025-12-30 01:17:45 -0500

    Integrate Material UI into frontend

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/index.css'
+import { ThemeProvider, CssBaseline } from '@mui/material'
+import theme from './styles/theme'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
