7c82385387fde06ee84b6e9f1cf15f95c0685d03
Jean-Paul Niko <jeanpaulniko@MacBook-Pro-M2-13.local>
2026-01-07 15:05:11 -0500

    Fix Ce.get is not a function error - add axios-style HTTP methods to api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,51 @@
-import apiClient from './apiClient';
+import { request } from '@musclemap/client';
 
-export const api = apiClient;
+/**
+ * Axios-style API wrapper using the @musclemap/client request function.
+ * Provides get, post, put, delete methods for legacy code compatibility.
+ */
+export const api = {
+  /**
+   * Make a GET request
+   * @param {string} path - API path
+   * @returns {Promise<{data: any}>} Response with data property
+   */
+  async get(path) {
+    const response = await request(path, { method: 'GET' });
+    return { data: response };
+  },
+
+  /**
+   * Make a POST request
+   * @param {string} path - API path
+   * @param {object} body - Request body
+   * @returns {Promise<{data: any}>} Response with data property
+   */
+  async post(path, body) {
+    const response = await request(path, { method: 'POST', body });
+    return { data: response };
+  },
+
+  /**
+   * Make a PUT request
+   * @param {string} path - API path
+   * @param {object} body - Request body
+   * @returns {Promise<{data: any}>} Response with data property
+   */
+  async put(path, body) {
+    const response = await request(path, { method: 'PUT', body });
+    return { data: response };
+  },
+
+  /**
+   * Make a DELETE request
+   * @param {string} path - API path
+   * @returns {Promise<{data: any}>} Response with data property
+   */
+  async delete(path) {
+    const response = await request(path, { method: 'DELETE' });
+    return { data: response };
+  },
+};
 
 export default api;
